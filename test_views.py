4a5f7281f6bb6a7cdd1f7c47422d94d56a03a0ab
Conrad Lukawski <conrad@backlight.ca>
2018-01-02 21:16:21 -0500

    Test deletion

--- test_views.py
+++ test_views.py
@@ -11,9 +11,21 @@ class ViewsTestCase(TestCase):
 
         response = views.index(request)
         
+        print(response)
         server = Server.objects.all()[0]
 
         self.assertEqual(response.status_code, 200)
         self.assertTrue("192.168.1.100" in response.content.decode("utf-8"))
         self.assertTrue("192.168.1.100", server.ip)
 
+    def test_delete_success(self):
+        # Create Server object
+        server = Server(ip='192.168.1.100', username='username', password='password')
+        self.assertTrue("192.168.1.100", server.ip)
+
+        request = self.factory.post('/delete/1')
+
+        response = views.delete(request, 1)
+
+        self.assertTrue("192.168.1.100" not in response.content.decode("utf-8"))
+        self.assertFalse(Server.objects.all())
